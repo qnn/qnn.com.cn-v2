d12d268dfaf36aab074763cc85d597b1a1d03f48
caiguanhao <caiguanhao@gmail.com>
2013-06-19 14:55:46 +0800

    improve product details page; add 10 products;

--- _parser/get_products.rb
+++ _parser/get_products.rb
@@ -4,9 +4,10 @@ require 'action_view'
 require 'fileutils'
 require 'sqlite3'
 
-posts_dir = "../_posts"
+posts_dir = "../products/_posts"
 
 begin
+  count = 0
   db = SQLite3::Database.open "database"
   stm = db.prepare "SELECT `id` FROM `8html_qnn_arctype` WHERE `topid`=12 OR `topid`=15"
   rs = stm.execute
@@ -22,7 +23,7 @@ begin
   rs.each do |row|
     open("#{posts_dir}/2013-1-1-#{row[3]}.md", "w") do |f|
       row[2].gsub!("\n","")
-      body = row[4].gsub(/"\.\.\/.+?image-pro/,"\"{{ site.prodimgs }}")
+      body = row[4].gsub(/"\.\.\/.+?image-pro/,"\"%PRODIMGS%")
       body = body.split("<hr />")
       specs = ActionView::Base.full_sanitizer.sanitize(body[0]).gsub(/^[\t\s]+/,"")
       images = ActionView::Base.full_sanitizer.sanitize(body[1].strip).gsub(/^[\t\s]+/,"").gsub(/.*\//,"").gsub(/^/,"  - ")
@@ -33,11 +34,12 @@ begin
       f.puts <<content
 ---
 layout: product_article
+permalink: \/#{row[3]}.html
+
 type: #{row[5]}
 title: #{row[0]}
 keywords: #{row[1]}
 description: #{row[2]}
-title: #{row[0]}
 
 specs: #{specs[/规格：(.*)/, 1]}
 thickness: #{specs[/厚度：(.*)/, 1]}
@@ -54,10 +56,11 @@ spec_details: |
 
 manual: |
 #{manual}
-
 ---
 content
     end
+    count = count + 1
+    exit if count > 10
   end
 rescue SQLite3::Exception => e
   puts "Exception occurred."
